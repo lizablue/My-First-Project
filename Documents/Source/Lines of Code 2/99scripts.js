37659f5bc07c1da06d6e9e11b9fe677d04463aaa
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-09 18:17:12 -0500

    add song lyrics and css styling

--- Documents/Source/Lines of Code 2/99scripts.js
+++ Documents/Source/Lines of Code 2/99scripts.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     // create button that tells friends to sing
     let button = document.createElement('button');
-    button.id = 'sing';
+    button.id = 'btn-sing';
+    let sing = document.createTextNode("sing!");
+    button.appendChild(sing);
     document.body.appendChild(button);
 
     // friends who sing
@@ -12,29 +14,28 @@ document.addEventListener('DOMContentLoaded', function () {
             let friendDiv = document.createElement('div');
             friendDiv.className = "friend";
             document.body.appendChild(friendDiv);
-            for (i in friends) {
-                let friendName = document.createTextNode(friends[i]);
-                let h3 = document.createElement('h3');
-                h3.appendChild(friendName);
-                document.body.appendChild(h3);
-            }
+            let friendName = document.createTextNode(friends[i]);
+            let h3 = document.createElement('h3');
+            h3.appendChild(friendName);
+            friendDiv.appendChild(h3);
 
             // song that is sung
-            for (let x = 0; x < friends.length; x++) {
-
-                for (let i = 9; i > 0; i--) {
-
-                    if (i === 2) {
-                        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " line of code in the file");
-                    } else if (i === 1) {
-                        console.log(i + " line of code in the file, " + i + " line of code; " + friends[x] + " strikes one out, clears it all out, no more lines of code in the file");
-                    } else {
-                        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file");
-                    }
+            for (let x = 99; x > 0; x--) {
+                let p = document.createElement('p');
+                friendDiv.appendChild(p);
+                if (x === 2) {
+                    let secondLast = document.createTextNode(x + " lines of code in the file, " + x + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (x - 1) + " line of code in the file");
+                    p.appendChild(secondLast);
+                } else if (x === 1) {
+                    let last = document.createTextNode(x + " line of code in the file, " + x + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+                    p.appendChild(last);
+                } else {
+                    let main = document.createTextNode(x + " lines of code in the file, " + x + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (x - 1) + " lines of code in the file");
+                    p.appendChild(main);
                 }
+
             }
         }
-
     });
 });
 
