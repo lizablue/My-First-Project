2363e525361a3c634ece97b060aad43afaa2d76c
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-09 17:53:24 -0500

    recreate HTML setup with DOM instead

--- Documents/Source/Lines of Code 2/99scripts.js
+++ Documents/Source/Lines of Code 2/99scripts.js
@@ -1,32 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // create button that tells friends to sing
     let button = document.createElement('button');
     button.id = 'sing';
     document.body.appendChild(button);
-    
-    // start singing on button click
-    button = document.getElementById('sing')
 
     // friends who sing
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         let friends = ['Ana', 'Laur', 'Faith', 'Kim', 'Steph'];
 
-        // song that is sung
-        for (let x = 0; x < friends.length; x++) {
+        for (let i = 0; i < friends.length; i++) {
+            let friendDiv = document.createElement('div');
+            friendDiv.className = "friend";
+            document.body.appendChild(friendDiv);
+            for (i in friends) {
+                let friendName = document.createTextNode(friends[i]);
+                let h3 = document.createElement('h3');
+                h3.appendChild(friendName);
+                document.body.appendChild(h3);
+            }
 
-            console.log(friends[x].toUpperCase() + ':');
-    
-            for (let i = 9; i > 0; i--) {
-    
-                if (i === 2) {
-                    console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " line of code in the file");
-                } else if (i === 1) {
-                    console.log(i + " line of code in the file, " + i + " line of code; " + friends[x] + " strikes one out, clears it all out, no more lines of code in the file");
-                } else {
-                    console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file");
+            // song that is sung
+            for (let x = 0; x < friends.length; x++) {
+
+                for (let i = 9; i > 0; i--) {
+
+                    if (i === 2) {
+                        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " line of code in the file");
+                    } else if (i === 1) {
+                        console.log(i + " line of code in the file, " + i + " line of code; " + friends[x] + " strikes one out, clears it all out, no more lines of code in the file");
+                    } else {
+                        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file");
+                    }
                 }
             }
         }
-    
+
     });
 });
 
