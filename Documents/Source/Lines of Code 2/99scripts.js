f8facf9fd7499b87ae810ab55e362252944b6442
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-09 17:40:18 -0500

    add button to make friends sing

--- Documents/Source/Lines of Code 2/99scripts.js
+++ Documents/Source/Lines of Code 2/99scripts.js
@@ -1,15 +1,32 @@
-let friends = ['Ana', 'Laur', 'Faith', 'Kim', 'Steph'];
+document.addEventListener('DOMContentLoaded', function () {
+    let button = document.createElement('button');
+    button.id = 'sing';
+    document.body.appendChild(button);
+    
+    // start singing on button click
+    button = document.getElementById('sing')
 
-for(var x = 0; x < friends.length; x++) {
-    for (var i = 99; i > 2; i--) {
-        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i-1) + " lines of code in the file")
-    };
-    // last couple lines were a little different because of singular/plural rules
-    // is there a more efficient way to do this?
-    if (i = 2) {
-        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i-1) + " line of code in the file")
-    };
-    if (i = 1) {
-        console.log(i + " line of code in the file, " + i + " line of code; " + friends[x] + " strikes one out, clears it all out, no more lines of code in the file")
-    };
-};
+    // friends who sing
+    button.addEventListener('click', function() {
+        let friends = ['Ana', 'Laur', 'Faith', 'Kim', 'Steph'];
+
+        // song that is sung
+        for (let x = 0; x < friends.length; x++) {
+
+            console.log(friends[x].toUpperCase() + ':');
+    
+            for (let i = 9; i > 0; i--) {
+    
+                if (i === 2) {
+                    console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " line of code in the file");
+                } else if (i === 1) {
+                    console.log(i + " line of code in the file, " + i + " line of code; " + friends[x] + " strikes one out, clears it all out, no more lines of code in the file");
+                } else {
+                    console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file");
+                }
+            }
+        }
+    
+    });
+});
+
